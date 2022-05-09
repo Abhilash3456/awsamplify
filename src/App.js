a19fcfa302764d0203f3e8b3fd99a61faee0e946
abhilash.subramanian@codincity.com <abhilash.subramanian@codincity.com>
2022-05-09 17:18:50 +0530

    updated the message second time

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          welcome to the react app
+          welcome to the react app for the first time
         </p>
         <a
           className="App-link"
